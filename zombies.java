97223a40f1cd08e2ed18bdaa61fafe79382398e8
idohalamit <idohalamit@gmail.com>
2020-01-31 13:24:39 +0200

    כתובות נפוצות שהזומבים קופצים אליהן
    שיניתי את התכנית ככה שהיא תגריל ערכים, ועשיתי מהערכים טבלה ותרשים אקסל.

---
+++ zombies.java
@@ -0,0 +1,63 @@
+package valuefinder;
+
+import java.util.*;
+
+public class zombies {
+	public static void main(String[] args) {
+		Random rando = new Random();
+		for (int c = 100000; c> 0; c--) {
+			int si = 0;
+
+			int bx = rando.nextInt();
+			bx &= 0xffff;
+			System.out.print(Integer.toHexString(bx));
+			int cx = rando.nextInt();
+			cx &= 0xffff;
+			System.out.print(", " + Integer.toHexString(cx));
+			int dx = rando.nextInt();
+			dx &= 0xffff;
+			System.out.print(", " + Integer.toHexString(dx));
+			int di = rando.nextInt();
+			di &= 0xffff;
+			System.out.print(", " + Integer.toHexString(di));
+
+			int bp = di ^ bx ^ cx ^ dx;
+			System.out.print(", " + Integer.toHexString(bp));
+
+			int ax = di; // 000000A8 89C7 mov di,ax
+
+			ax ^= bx; // 000000AA 31D8 xor ax,bx
+			ax ^= cx; // 000000AC 31C8 xor ax,cx
+			ax ^= dx; // 000000AE 31D0 xor ax,dx
+
+			ax = di; // 000000B4 89F8 mov ax,di
+			ax = ax & 0x5555; // 000000B6 255555 and ax,0x5555
+			si = bx; // 000000B9 89DE mov si,bx
+			si = si & 0x5555; // 000000BB 81E65555 and si,0x5555
+			ax += si; // 000000BF 01F0 add ax,si
+			ax &= 0xffff;
+			si = cx; // 000000C1 89CE mov si,cx
+			si = si & 0x5555; // 000000C3 81E65555 and si,0x5555
+			ax += si; // 000000C7 01F0 add ax,si
+			ax &= 0xffff;
+			si = dx; // 000000C9 89D6 mov si,dx
+			si = si & 0x5555; // 000000CB 81E65555 and si,0x5555
+			ax += si; // 000000CF 01F0 add ax,si
+			ax &= 0xffff;
+			si = bp; // 000000D1 89EE mov si,bp
+			si = si & 0x5555; // 000000D3 81E65555 and si,0x5555
+			ax += si; // 000000D7 01F0 add ax,si
+			ax &= 0xffff;
+			ax = ax >> 1; // 000000D9 D1E8 shr ax,1
+			ax &= 0xffff;
+			ax &= 0xaaaa; // 000000DB 25AAAA and ax,0xaaaa
+			ax -= 0x2019; // 000000DE 2D1920 sub ax,0x2019
+			ax &= 0xffff;
+
+			System.out.println(" : " + Integer.toString(ax)); // 000000E1 FFE0 jmp ax
+
+		}
+
+	}
+
+}
